4cb2faa6a2b889adbba95195d014f95712fa9981
kevinscircle@aol.com <kevingeoreyes@gmail.com>
2024-11-20 17:17:21 -0800

    base else not responding

--- posts/urls.py
+++ posts/urls.py
@@ -16,4 +16,10 @@ urlpatterns = [
     path("<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
     path("<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
     path("new/", PostCreateView.as_view(), name="post_create"),
+    path("drafts/", DraftPostListView.as_view(), name="draft_list"),
+    path("archived/", ArchivedPostListView.as_view(), name="archived_list"),
+
 ]
+
+
+# check my draft and archived urls and name field & where to connect in views
